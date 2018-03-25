4bf69ea63583bd69ed17196d5c406003512c01fa
hisham waleed karam <hisham.karam@cartologic.com>
2018-03-26 00:38:04 +0200

    add channels

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
+
+var c1 = make(chan string)
 
 // RoutineFunc is a routine function
 func RoutineFunc(caller string) {
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%s \t-\t %d \t on: %s \n", caller, i, time.Now().Format(time.RFC850))
+		msg := []string{caller, strconv.Itoa(i), "on: ", string(time.Now().Format(time.RFC850)), "\n"}
+		c1 <- strings.Join(msg, "\t")
 	}
 }
 func main() {
 	go RoutineFunc("caller1")
 	go RoutineFunc("caller2")
+	for i := 0; i < 20; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Printf("received - \t %s", msg1)
+		}
+	}
+
 	fmt.Println("Press <enter> key to close")
 	fmt.Scanln()
 	fmt.Println("Terminated")
